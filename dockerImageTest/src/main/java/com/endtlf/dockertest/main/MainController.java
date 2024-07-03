977e07960d5bce0878bf6b48c7c9f497ea640256
jisoo78 <ekelldks@naver.com>
2024-07-03 09:45:39 +0900

    images

---
+++ dockerImageTest/src/main/java/com/endtlf/dockertest/main/MainController.java
@@ -0,0 +1,14 @@
+package com.endtlf.dockertest.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main() {
+
+        return "main/main";
+    }
+}
---
+++ dockerImageTest/src/test/java/com/endtlf/dockertest/DockerImageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endtlf.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
